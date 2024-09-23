6cd71ff578106aab6dd421ce638c7287f87ac8c2
awes0meeman <39201831+Awes0meEman@users.noreply.github.com>
2024-09-23 15:19:49 -0400

    add models

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,2 +1,44 @@
 package models
 
+type Game struct {
+	Id			int		`json:"id"`
+	Title 		string 	`json:"title"`
+	Description string 	`json:"description"`
+	Currencies	[]Currency
+	Shops 		[]Shop
+}
+
+type Currency struct {
+	Id 			int 	`json:"id"`
+	GameId		int 	`json:"gameid"`
+	Name 		string 	`json:"name"`
+	Description string 	`json:"description"`
+}
+
+type Shop struct {
+	Id			int		`json:"id"`
+	GameId		int 	`json:"gameid"`
+	Title 		string 	`json:"title"`
+	Description string 	`json:"description"`
+	Items		[]ItemStock
+}
+
+type ItemStock struct {
+	Id 			int 	`json:"id"`
+	ItemId		int 	`json:"itemid"`
+	StockCount	int		`json:"stockcount"`
+}
+
+type Item struct {
+	Id			int	 	`json:"id"`
+	ShopId 		int 	`json:"shopid"`
+	CurrencyId 	int 	`json:"currencyid"`
+	Name 		string 	`json:"name"`
+	Description string 	`json:"description"`
+	BaseCost 	int 	`json:"cost"`
+	StockLimit	bool	`json:"stocklimit"`
+	IsRurual	bool	`json:"isrural"`
+	IsUrban 	bool	`json:"isurban"`
+	IsPremium 	bool	`json:"ispremium"`
+	StockCount	int 	`json:"stockcount"`
+}
